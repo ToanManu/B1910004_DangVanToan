a9c467d9702fd87b0c9962ae77403973fb9baa78
no <abc@gmail.com>
2023-10-05 14:55:39 +0700

    lab2

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 
-const ApiError = require("./app/api-error");
+const ApiError = require("./app/api-error") 
+
+const app = express();
 
 const contactsRouter = require("./app/routes/contact.route");
-const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts",contactsRouter);
 
-//handle 404 response
-app.use((req, res, next)=> {
-    return next (new ApiError(404,"Resource not found"));
+app.get("/", (req, res) => {
+    res.json({message: "Welcome to contactbook application!"})
 });
-app.use((err,req, res, next)=> {
+
+app.use("/api/contacts", contactsRouter)
+
+app.use((req, res, next)=>{
+    return next(new ApiError(404, "Resource not found"))
+})
+
+app.use((error, req, res, next) => {
     return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
-    });
-});
+        message: error.message || "Internal server error",
+    })
+})
 
-app.get("/", (req, res)=> {
-    res.json({ message: "Welcome to contact book application."});
-});
-
-module.exports =app;
+module.exports = app;
